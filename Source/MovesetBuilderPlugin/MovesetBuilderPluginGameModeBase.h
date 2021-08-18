73da409d0934f18b2105e131f122301a434bb098
Danil Loik <wconr@mail.ru>
2021-08-18 23:54:20 +0300

    Create project

---
+++ Source/MovesetBuilderPlugin/MovesetBuilderPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MovesetBuilderPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVESETBUILDERPLUGIN_API AMovesetBuilderPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
